df5f940e974e7e843e7e6094ec83d5308d49a9c8
chuk-chuk <yulia.v.litvin@gmail.com>
2017-07-07 11:56:15 +0100

    added private methods full & empty

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -9,13 +9,23 @@ class DockingStation
   end
 
   def release_bike
-    fail("I do apologise, there are no bikes available") if @bikes.empty?
+    fail("I do apologise, there are no bikes available") if empty?
     @bikes.pop
   end
 
   def dock bike
-    fail("Very sorry, but this station is already full") if @bikes.length >= 20
+    fail("Very sorry, but this station is already full") if full?
     @bikes << bike
+  end
+
+  private
+
+  def full?
+    @bikes.length >= 20
+  end
+
+  def empty?
+    @bikes.empty?
   end
 
 end
